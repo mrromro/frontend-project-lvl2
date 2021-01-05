8bdfa40762859bf6ff53bec0c5ba2023720763b7
Roman Rovin <insigniasane@gmail.com>
2021-01-05 16:23:29 +0300

    refactor

---
+++ cli.js
@@ -0,0 +1,19 @@
+import { program } from 'commander';
+
+program
+  .description(
+    'Compares two configuration files and shows a difference.',
+  )
+  .version(
+    '0.1.0',
+    '-V, --version',
+    'output the current version',
+  );
+
+program
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
+
+program.parse(process.argv);
+
+export default program;
--- gendiff.js
+++ gendiff.js
@@ -1,19 +1,3 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
-
-program
-  .description(
-    'Compares two configuration files and shows a difference.',
-  )
-  .version(
-    '0.1.0',
-    '-V, --version',
-    'output the current version',
-  );
-
-program
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format');
-
-program.parse(process.argv);
+import program from './cli.js';
