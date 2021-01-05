1d3395bafa6d0056ea4294404e9e61f820dbfc53
Roman Rovin <insigniasane@gmail.com>
2021-01-05 23:23:08 +0300

    process with no parsing fo output

--- gendiff.js
+++ gendiff.js
@@ -12,18 +12,58 @@ function filesToJSON(...files) {
     .map((content) => JSON.parse(content));
 }
 
+function isAdded({ key, collection, value }) {
+  return !(key in collection) && value !== undefined;
+}
+
+function isDeleted({ key, collection, value }) {
+  return key in collection && value === undefined;
+}
+
+function isUnchanged({ key, collection, value }) {
+  return key in collection && value === collection[key];
+}
+
+function isModified({ key, collection, value }) {
+  return key in collection && value !== collection[key];
+}
+
+const router = {
+  added: isAdded,
+  deleted: isDeleted,
+  unchanged: isUnchanged,
+  modified: isModified,
+};
+
+function testKey(typeRouter, testee) {
+  return Object.keys(typeRouter).find((type) => typeRouter[type](testee));
+}
+
+function makePaylosd(type, { key, collection, value }) {
+  switch (type) {
+    case 'added': return { [key]: value };
+    case 'deleted': return { [key]: collection[key] };
+    case 'unchanged': return { [key]: collection[key] };
+    case 'modified': return { [key]: collection[key], changed: value };
+    default:
+      throw new Error('unknown happens');
+  }
+}
+
 function compareObjects(...objects) {
-  const indent = '  ';
   const keys = getUniqKeys(objects);
   const [first, ...rest] = objects;
-  const result = keys.reduce((res, key) => {
-    const value = findLastValue(key, rest);
-    if (!(key in first)) return [...res, `+ ${key}: ${value}`];
-    if (value === undefined) return [...res, `- ${key}: ${value}`];
-    if (first[key] === value) return [...res, `  ${key}: ${value}`];
-    return [...res, `- ${key}: ${first[key]}`, `+ ${key}: ${value}`];
-  }, []);
-  return ['{\n', result.join(`\n${indent}`), '\n}'].join(indent);
+  const result = keys.map((key) => {
+    const testee = {
+      key,
+      collection: first,
+      value: findLastValue(key, rest),
+    };
+    const type = testKey(router, testee);
+    const payload = makePaylosd(type, testee);
+    return { type, payload };
+  });
+  return result;
 }
 
 program.action((filepath1, filepath2) => {
