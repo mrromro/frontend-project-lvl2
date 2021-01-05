56a267721198a5fc1aa08094932e6a8345c11bb9
Roman Rovin <insigniasane@gmail.com>
2021-01-05 23:44:17 +0300

    Refactor objects processing

---
+++ Router.js
@@ -0,0 +1,51 @@
+class Router {
+  constructor(router) {
+    this.router = router;
+  }
+
+  testKey(testee) {
+    return Object.keys(this.router).find((type) => this.router[type](testee));
+  }
+
+  static makePaylosd(type, { key, collection, value }) {
+    switch (type) {
+      case 'added': return { [key]: value };
+      case 'deleted': return { [key]: collection[key] };
+      case 'unchanged': return { [key]: collection[key] };
+      case 'modified': return { [key]: collection[key], changed: value };
+      default:
+        throw new Error('unknown happens');
+    }
+  }
+
+  process(testee) {
+    const type = this.testKey(testee);
+    const payload = this.constructor.makePaylosd(type, testee);
+    return { type, payload };
+  }
+}
+
+function isAdded({ key, collection, value }) {
+  return !(key in collection) && value !== undefined;
+}
+
+function isDeleted({ key, collection, value }) {
+  return key in collection && value === undefined;
+}
+
+function isUnchanged({ key, collection, value }) {
+  return key in collection && value === collection[key];
+}
+
+function isModified({ key, collection, value }) {
+  return key in collection && value !== collection[key];
+}
+
+const router = {
+  added: isAdded,
+  deleted: isDeleted,
+  unchanged: isUnchanged,
+  modified: isModified,
+};
+
+export default new Router(router);
--- gendiff.js
+++ gendiff.js
@@ -4,50 +4,13 @@ import { resolve } from 'path';
 import { readFileSync } from 'fs';
 import program, { output } from './cli.js';
 import { getUniqKeys, findLastValue } from './utils.js';
+import router from './Router.js';
 
 function filesToJSON(...files) {
   return files
     .map((file) => resolve(file))
     .map((filepath) => readFileSync(filepath, 'utf-8'))
     .map((content) => JSON.parse(content));
-}
-
-function isAdded({ key, collection, value }) {
-  return !(key in collection) && value !== undefined;
-}
-
-function isDeleted({ key, collection, value }) {
-  return key in collection && value === undefined;
-}
-
-function isUnchanged({ key, collection, value }) {
-  return key in collection && value === collection[key];
-}
-
-function isModified({ key, collection, value }) {
-  return key in collection && value !== collection[key];
-}
-
-const router = {
-  added: isAdded,
-  deleted: isDeleted,
-  unchanged: isUnchanged,
-  modified: isModified,
-};
-
-function testKey(typeRouter, testee) {
-  return Object.keys(typeRouter).find((type) => typeRouter[type](testee));
-}
-
-function makePaylosd(type, { key, collection, value }) {
-  switch (type) {
-    case 'added': return { [key]: value };
-    case 'deleted': return { [key]: collection[key] };
-    case 'unchanged': return { [key]: collection[key] };
-    case 'modified': return { [key]: collection[key], changed: value };
-    default:
-      throw new Error('unknown happens');
-  }
 }
 
 function compareObjects(...objects) {
@@ -59,9 +22,7 @@ function compareObjects(...objects) {
       collection: first,
       value: findLastValue(key, rest),
     };
-    const type = testKey(router, testee);
-    const payload = makePaylosd(type, testee);
-    return { type, payload };
+    return router.process(testee);
   });
   return result;
 }
