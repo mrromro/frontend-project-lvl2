577214eff91142767a898c7ec9ed901864c2ccab
Roman Rovin <insigniasane@gmail.com>
2021-01-05 15:47:22 +0300

    add support cli

--- gendiff.js
+++ gendiff.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .description(
+    'Compares two configuration files and shows a difference.',
+  )
+  .version(
+    '0.1.0',
+    '-V, --version',
+    'output the current version',
+  )
+  .parse(process.argv);
