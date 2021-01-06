318db73f0965b6c78145507a18b37cde436a7167
Roman Rovin <insigniasane@gmail.com>
2021-01-06 17:33:02 +0300

    pass utility from gendiff to utils

--- gendiff.js
+++ gendiff.js
@@ -1,33 +1,24 @@
 #!/usr/bin/env node
 
-import { resolve } from 'path';
-import { readFileSync } from 'fs';
 import program, { output } from './cli.js';
-import { getUniqKeys, findLastValue } from './utils.js';
+import utils from './utils.js';
 import router from './Router.js';
 
-function filesToJSON(...files) {
-  return files
-    .map((file) => resolve(file))
-    .map((filepath) => readFileSync(filepath, 'utf-8'))
-    .map((content) => JSON.parse(content));
-}
-
 function compareObjects(...objects) {
-  const keys = getUniqKeys(objects);
+  const keys = utils.getUniqKeys(objects);
   const [first, ...rest] = objects;
   return keys.map((key) => {
     const testee = {
       key,
       collection: first,
-      value: findLastValue(key, rest),
+      value: utils.findLastValue(key, rest),
     };
     return router.process(testee);
   });
 }
 
 program.action((filepath1, filepath2) => {
-  const [file1, file2] = filesToJSON(filepath1, filepath2);
+  const [file1, file2] = utils.JSONfilesToObjects(filepath1, filepath2);
   output(compareObjects(file1, file2));
 });
 
--- utils.js
+++ utils.js
@@ -1,3 +1,6 @@
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
+
 function getUniqKeys(objects, sort = true) {
   const keys = [...new Set(...objects.map((obj) => Object.keys(obj)))];
   return sort ? keys.sort() : keys;
@@ -9,4 +12,15 @@ function findLastValue(key, collection) {
   return obj ? obj[key] : undefined;
 }
 
-export { getUniqKeys, findLastValue };
+function JSONfilesToObjects(...files) {
+  return files
+    .map((file) => resolve(file))
+    .map((filepath) => readFileSync(filepath, 'utf-8'))
+    .map((content) => JSON.parse(content));
+}
+
+export default {
+  getUniqKeys,
+  findLastValue,
+  JSONfilesToObjects,
+};
