9a0faa12c166e62a82eec7f0c0327f4baad85ba3
Roman Rovin <insigniasane@gmail.com>
2021-01-07 23:08:11 +0300

    Parser test to refactor

--- __tests__/Parser.test.js
+++ __tests__/Parser.test.js
@@ -1,145 +1,123 @@
 import { describe, expect, test } from '@jest/globals';
 import parser from '../src/Parser.js';
 
+const predicateTest = ((router) => (testee) => {
+  const entries = Object.entries(router);
+  return entries.reduce((acc, [key, predicate]) => {
+    if (predicate(testee)) return [...acc, key];
+    return acc;
+  }, []);
+})(parser.router);
+
+const testeesBook = {
+  added: {
+    key: 'key',
+    value: 'value',
+    collection: {},
+  },
+  deleted: {
+    key: 'key',
+    value: undefined,
+    collection: { key: 'value' },
+  },
+  modified: {
+    key: 'key',
+    value: 'value 2',
+    collection: { key: 'value' },
+  },
+  unchanged: {
+    key: 'key',
+    value: 'value',
+    collection: { key: 'value' },
+  },
+};
+
+const results = {};
+
 describe('Router functions test suit', () => {
   describe('added test', () => {
-    const testee = {
-      key: 'key',
-      value: 'value',
-      collection: {},
-    };
-
     const type = 'added';
-    let payload;
+    const { [type]: testee } = testeesBook;
 
     test('predicates test', () => {
-      const added = parser.router.added(testee);
-      const deleted = parser.router.deleted(testee);
-      const modified = parser.router.modified(testee);
-      const unchanged = parser.router.unchanged(testee);
-      expect(added).toBe(true);
-      expect(deleted).toBe(false);
-      expect(modified).toBe(false);
-      expect(unchanged).toBe(false);
+      const received = predicateTest(testee);
+      expect(received).toStrictEqual([type]);
     });
     test('router test', () => {
       const routed = parser.testKey(testee);
       expect(routed).toBe(type);
     });
     test('makePayload test', () => {
-      payload = parser.constructor.makePayload(type, testee);
+      results[type] = parser.constructor.makePayload(type, testee);
       const { key, value } = testee;
-      expect(payload).toStrictEqual({ [key]: value });
-    });
-    test('process test', () => {
-      const received = parser.process(testee);
-      expect(received).toStrictEqual({ type, payload });
+      expect(results[type]).toStrictEqual({ [key]: value });
     });
   });
 
   describe('deleted test', () => {
-    const testee = {
-      key: 'key',
-      value: undefined,
-      collection: { key: 'value' },
-    };
-
     const type = 'deleted';
-    let payload;
+    const { [type]: testee } = testeesBook;
 
     test('predicates test', () => {
-      const added = parser.router.added(testee);
-      const deleted = parser.router.deleted(testee);
-      const modified = parser.router.modified(testee);
-      const unchanged = parser.router.unchanged(testee);
-      expect(added).toBe(false);
-      expect(deleted).toBe(true);
-      expect(modified).toBe(false);
-      expect(unchanged).toBe(false);
+      const received = predicateTest(testee);
+      expect(received).toStrictEqual([type]);
     });
     test('router test', () => {
       const routed = parser.testKey(testee);
       expect(routed).toBe(type);
     });
     test('makePayload test', () => {
-      payload = parser.constructor.makePayload(type, testee);
+      results[type] = parser.constructor.makePayload(type, testee);
       const { key, collection } = testee;
-      expect(payload).toStrictEqual({ [key]: collection[key] });
-    });
-    test('process test', () => {
-      const received = parser.process(testee);
-      expect(received).toStrictEqual({ type, payload });
+      expect(results[type]).toStrictEqual({ [key]: collection[key] });
     });
   });
 
   describe('modified test', () => {
-    const testee = {
-      key: 'key',
-      value: 'value 2',
-      collection: { key: 'value' },
-    };
-
     const type = 'modified';
-    let payload;
+    const { [type]: testee } = testeesBook;
 
     test('predicates test', () => {
-      const added = parser.router.added(testee);
-      const deleted = parser.router.deleted(testee);
-      const modified = parser.router.modified(testee);
-      const unchanged = parser.router.unchanged(testee);
-      expect(added).toBe(false);
-      expect(deleted).toBe(false);
-      expect(modified).toBe(true);
-      expect(unchanged).toBe(false);
+      const received = predicateTest(testee);
+      expect(received).toStrictEqual([type]);
     });
     test('router test', () => {
       const routed = parser.testKey(testee);
       expect(routed).toBe(type);
     });
     test('makePayload test', () => {
-      payload = parser.constructor.makePayload(type, testee);
+      results[type] = parser.constructor.makePayload(type, testee);
       const { key, collection, value } = testee;
-      expect(payload).toStrictEqual({ [key]: collection[key], changed: value });
-    });
-    test('process test', () => {
-      const received = parser.process(testee);
-      expect(received).toStrictEqual({ type, payload });
+      expect(results[type]).toStrictEqual({ [key]: collection[key], changed: value });
     });
   });
 
   describe('unchanged test', () => {
-    const testee = {
-      key: 'key',
-      value: 'value',
-      collection: { key: 'value' },
-    };
-
     const type = 'unchanged';
-    let payload;
+    const { [type]: testee } = testeesBook;
 
     test('isUnchanged test', () => {
-      const added = parser.router.added(testee);
-      const deleted = parser.router.deleted(testee);
-      const modified = parser.router.modified(testee);
-      const unchanged = parser.router.unchanged(testee);
-      expect(added).toBe(false);
-      expect(deleted).toBe(false);
-      expect(modified).toBe(false);
-      expect(unchanged).toBe(true);
+      const received = predicateTest(testee);
+      expect(received).toStrictEqual([type]);
     });
     test('router test', () => {
       const routed = parser.testKey(testee);
       expect(routed).toBe(type);
     });
     test('makePayload test', () => {
-      payload = parser.constructor.makePayload(type, testee);
+      results[type] = parser.constructor.makePayload(type, testee);
       const { key, collection, value } = testee;
-      expect(payload).toStrictEqual({ [key]: value });
-      expect(payload).toStrictEqual({ [key]: collection[key] });
+      expect(results[type]).toStrictEqual({ [key]: value });
+      expect(results[type]).toStrictEqual({ [key]: collection[key] });
     });
-    test('process test', () => {
-      const received = parser.process(testee);
-      expect(received).toStrictEqual({ type, payload });
+  });
+
+  test('process test', () => {
+    const types = Object.keys(testeesBook);
+    types.forEach((type) => {
+      const { [type]: testee } = testeesBook;
+      const { [type]: payload } = results;
+      expect(parser.process(testee)).toStrictEqual({ type, payload });
     });
   });
 });
