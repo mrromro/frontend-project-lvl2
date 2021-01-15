3f3038dfd61ca46245b28680d3aafbcac1540146
Roman Rovin <insigniasane@gmail.com>
2021-01-16 01:48:30 +0300

    Create stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,1 +1,25 @@
-export default (record) => Boolean(record);
+const templates = {
+  added: '+',
+  deleted: '-',
+  unchanged: ' ',
+  object: (obj, level = 0) => `{\n${obj}\n${'  '.repeat(level)}}`,
+};
+
+const makeRecord = (node, level = 0, pad = '  ') => {
+  const { key, value, type } = node;
+  const indent = pad.repeat(level);
+  const record = `${templates[type]} ${key}: ${value}`;
+  return indent + record;
+};
+
+const formatter = (tier, level = 0) => {
+  if (typeof tier !== 'object') return tier;
+  const outcome = tier.map((node) => {
+    const { key, type, value } = node;
+    const data = { key, type, value: formatter(value, level + 2) };
+    return makeRecord(data, level + 1);
+  });
+  return templates.object(outcome.join('\n'), level);
+};
+
+export default formatter;
