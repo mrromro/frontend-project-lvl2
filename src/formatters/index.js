0a78604bc47548e881c13de1520ce67e7f954347
Roman Rovin <insigniasane@gmail.com>
2021-01-17 17:39:20 +0300

    Adjust formatters loader

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,8 +1,11 @@
 const router = {
-  stylish: './stylish.js',
+  get default() { return this.stylish; },
+  get stylish() { return './stylish.js'; },
+  get plain() { return './plain.js'; },
 };
 
 export default async (format) => {
-  const { default: formatter } = await import(router[format]);
+  const path = router[format] || router.default;
+  const { default: formatter } = await import(path);
   return formatter;
 };
