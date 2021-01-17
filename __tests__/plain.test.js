6d627ac5da9cf8a7da422e457e216bdd3cd7924f
Roman Rovin <insigniasane@gmail.com>
2021-01-17 20:45:18 +0300

    Fix tests

--- __tests__/plain.test.js
+++ __tests__/plain.test.js
@@ -19,7 +19,7 @@ describe('plain formatter test', () => {
     ];
     const received = formatter(tree);
     const expected = [
-      "Property 'key2' was added with value: value2",
+      "Property 'key2' was added with value: 'value2'",
       "Property 'key3' was removed",
     ].join('\n');
     expect(received).toBe(expected);
@@ -32,14 +32,44 @@ describe('plain formatter test', () => {
           { key: 'key2', value: 'value2', type: 'added' },
           { key: 'key3', value: 'value3', type: 'removed' },
         ],
-        type: 'unchanged',
       },
       { key: 'key4', value: 'value4' },
     ];
     const received = formatter(tree);
     const expected = [
-      "Property 'key1.key2' was added with value: value2",
+      "Property 'key1.key2' was added with value: 'value2'",
       "Property 'key1.key3' was removed",
+    ].join('\n');
+    expect(received).toBe(expected);
+  });
+  test('nested tree extended test', () => {
+    const tree = [
+      {
+        key: 'key1',
+        value: [
+          { key: 'key2', value: 'value2', type: 'added' },
+          {
+            key: 'key3',
+            value: 'value3',
+            newValue: 'value33',
+            type: 'updated',
+          },
+          { key: 'key6', value: 'value6' },
+        ],
+      },
+      { key: 'key4', value: 'value4' },
+      {
+        key: 'key5',
+        value: 'value5',
+        newValue: 'value5-updated',
+        type: 'updated',
+      },
+    ];
+    const received = formatter(tree);
+    const expected = [
+      "Property 'key1.key2' was added with value: 'value2'",
+      "Property 'key1.key3' was updated. From 'value3' to 'value33'",
+      "Property 'key5' was updated. From 'value5' to 'value5-updated'",
     ].join('\n');
     expect(received).toBe(expected);
   });
